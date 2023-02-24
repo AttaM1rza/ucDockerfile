21da71f804affde11463da06b040edf4b7b9d5b9
Atta Mirza <101592061+AttaM1rza@users.noreply.github.com>
2023-02-24 15:10:48 +0100

    Update demo.py

--- demo.py
+++ demo.py
@@ -7,13 +7,24 @@ import undetected_chromedriver as uc
 sleepTime = 5
 url = "https://nowsecure.nl/#relax"
 url = "https://www.zara.com/de/de/jacke-aus-kunstleder-p08281450.html?v1=222756772"
+VERSION_MAIN = 108
 
+def getCompatibleChromeVersion():
+  str1 = driver.capabilities['browserVersion']
+  str2 = driver.capabilities['chrome']['chromedriverVersion'].split(' ')[0]
+  print(str1)
+  print(str2)
+  print(str1[0:2])
+  print(str2[0:2])
+  if str1[0:2] != str2[0:2]: 
+    print("please download correct chromedriver version")
+  
 logging.basicConfig(level=10)
-logging.getLogger("parso").setLevel(100) # i hate damn parso
+logging.getLogger("parso").setLevel(100)
 
 #o = uc.ChromeOptions()
 #o.arguments.extend(["--no-sandbox", "--disable-setuid-sandbox"])  # these are needed to run chrome as root
-driver = uc.Chrome(advanced_elements=True, version_main = 110)  # driver = uc.Chrome( version_main = 108 )
+driver = uc.Chrome(advanced_elements=True, version_main = VERSION_MAIN)  # driver = uc.Chrome( version_main = 108 )
 driver.get(url)
 
 logging.getLogger().info(f'sleeping {sleepTime} seconds to give site a chance to load')
